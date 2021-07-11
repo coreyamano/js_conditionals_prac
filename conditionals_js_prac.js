1497c7d16575ec29e6a251facc8d90e8f285b514
Corey <corey.amano@gmail.com>
2021-07-10 23:02:07 -0700

    solve problems 7-10

--- conditionals_js_prac.js
+++ conditionals_js_prac.js
@@ -62,9 +62,45 @@ if (num1 > 10 || num2 > 10){
 
 // # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
 
+var number = 12
+if (number < 0){
+  console.log(1776)
+} else {
+  console.log(1979)
+}
+
 // # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+var x = 99
+if (x == 100) {
+  console.log(100)
+} else if (x == 99) {
+  console.log(99)
+} else {
+  console.log(0)
+}
 
 // # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
 
+var num1 = 22
+var num2 = -14
+if (num1 < 0 && num2 > 0){
+  log.console(1)
+} else {
+  console.log(0)
+}
+
 // # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 
+var y = 36
+if(y > 80) {
+  console.log(5)
+} else if (y > 60) {
+  console.log(4)
+} else if (y > 40) {
+  console.log(3)
+} else if (y > 20) {
+  console.log(2)
+} else {
+  console.log(1)
+}
